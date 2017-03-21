4803c6ee1002f35da899c1302c7e45f83593de77
lsankar4033 <lsankar4033@gmail.com>
2017-03-21 09:37:18 +0800

    Add segment combining to `gridland_metro`

--- hackerrank/gridland_metro/run.py
+++ hackerrank/gridland_metro/run.py
@@ -2,12 +2,29 @@
 import sys
 
 from collections import defaultdict
+from itertools import filterfalse
 
 # Return a new list of segments
 def add_segment(existing_segments, new_segment):
-    # get segments that intersect with both front and back and properly take the endpoints
 
-    return []
+    def segment_intersects(segment):
+        (sc1, sc2) = segment
+        return (sc2 >= nc1 and sc2 <= nc2) or (sc1 >= nc1 and sc1 <= nc2)
+
+    (nc1, nc2) = new_segment
+
+    intersecting = list(filter(segment_intersects, existing_segments))
+    non_intersecting = list(filterfalse(segment_intersects, existing_segments))
+
+    if len(intersecting) == 0:
+        new_c1 = nc1
+        new_c2 = nc2
+    else:
+        new_c1 = min([c1 for (c1, c2) in intersecting] + [nc1])
+        new_c2 = max([c2 for (c1, c2) in intersecting] + [nc2])
+
+    non_intersecting.append((new_c1, new_c2))
+    return non_intersecting
 
 # each train track is a tuple of (r, c1, c2)
 def get_num_used_squares(train_tracks):
