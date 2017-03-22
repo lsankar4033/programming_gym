aab072e6493c48119dde1a004f17a411e248d28c
lsankar4033 <lsankar4033@gmail.com>
2017-03-22 12:57:03 +0800

    Finish up gridland_metro

--- hackerrank/gridland_metro/run.py
+++ hackerrank/gridland_metro/run.py
@@ -6,22 +6,20 @@ from itertools import filterfalse
 
 # Return a new list of segments
 def add_segment(existing_segments, new_segment):
-
     def segment_intersects(segment):
         (sc1, sc2) = segment
-        return (sc2 >= nc1 and sc2 <= nc2) or (sc1 >= nc1 and sc1 <= nc2)
+        # TODO - make this a multi-line or. I didn't have wifi when I solved this so couldn't look it up...
+        new_intersects = (nc1 >= sc1 and nc1 <= sc2) or (nc2 >= sc1 and nc2 <= sc2)
+        old_intersects = (sc2 >= nc1 and sc2 <= nc2) or (sc1 >= nc1 and sc1 <= nc2)
+        return new_intersects or old_intersects
 
     (nc1, nc2) = new_segment
 
     intersecting = list(filter(segment_intersects, existing_segments))
     non_intersecting = list(filterfalse(segment_intersects, existing_segments))
 
-    if len(intersecting) == 0:
-        new_c1 = nc1
-        new_c2 = nc2
-    else:
-        new_c1 = min([c1 for (c1, c2) in intersecting] + [nc1])
-        new_c2 = max([c2 for (c1, c2) in intersecting] + [nc2])
+    new_c1 = min([c1 for (c1, c2) in intersecting] + [nc1])
+    new_c2 = max([c2 for (c1, c2) in intersecting] + [nc2])
 
     non_intersecting.append((new_c1, new_c2))
     return non_intersecting
@@ -35,8 +33,12 @@ def get_num_used_squares(train_tracks):
         row_to_segments[r] = add_segment(row_to_segments[r], (c1, c2))
 
     # count used cells by going through map
+    num_cells = 0
+    for _, segments in row_to_segments.items():
+        for (c1, c2) in segments:
+            num_cells += (c2 + 1) - c1
 
-    pass
+    return num_cells
 
 # NOTE - this method is just copied around in all hackerrank modules
 def get_tuple_from_stdin():
@@ -44,4 +46,11 @@ def get_tuple_from_stdin():
     return [int(i) for i in l.split(" ")]
 
 if __name__ == "__main__":
-    pass
+    (r, c, num_tracks) = get_tuple_from_stdin()
+
+    train_tracks = []
+    for i in range(num_tracks):
+        train_tracks.append(get_tuple_from_stdin())
+
+    num_available_squares = (r * c) - get_num_used_squares(train_tracks)
+    print(num_available_squares)
