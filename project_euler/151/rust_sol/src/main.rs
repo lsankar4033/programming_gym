674baf209d2ec44fa9a27a9a95d255bb2e1aa380
lsankar4033 <lsankar4033@gmail.com>
2019-11-23 23:11:32 -0800

    Probably almost working?

--- project_euler/151/rust_sol/src/main.rs
+++ project_euler/151/rust_sol/src/main.rs
@@ -1,8 +1,5 @@
-// t := 0
-// for k = 1 up to k = 500500:
-//     t := (615949*t + 797807) modulo 220
-//     sk := t−219
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn generate_triangle(size: usize) -> Vec<i64> {
     let mut triangle: Vec<i64> = Vec::new();
@@ -73,6 +70,19 @@ struct Problem {
 
     row_col_map: HashMap<(u32, u32), usize>,
 }
+impl Problem {
+    fn size(&self) -> usize {
+        self.size()
+    }
+
+    fn num_rows(&self) -> usize {
+        let mut unique_rows: HashSet<u32> = HashSet::new();
+        for i in 0..self.rows.len() {
+            unique_rows.insert(self.rows[i]);
+        }
+        unique_rows.len()
+    }
+}
 
 fn generate_problem(size: usize) -> Problem {
     let triangle = generate_triangle(size);
@@ -97,6 +107,8 @@ mod test {
     fn test_generate_problem_1() {
         let problem = generate_problem(1);
 
+        assert_eq!(problem.size(), 1);
+        assert_eq!(problem.num_rows(), 1);
         assert_eq!(problem.triangle, [273519]);
         assert_eq!(problem.rows, [0]);
         assert_eq!(problem.cols, [0]);
@@ -107,6 +119,8 @@ mod test {
     fn test_generate_problem_3() {
         let problem = generate_problem(3);
 
+        assert_eq!(problem.size(), 3);
+        assert_eq!(problem.num_rows(), 2);
         assert_eq!(problem.triangle, [273519, -153582, 450905]);
         assert_eq!(problem.rows, [0, 1, 1]);
         assert_eq!(problem.cols, [0, 0, 1]);
@@ -118,29 +132,65 @@ mod test {
 
 // exactly 1000 rows
 struct Solutions {
-    map: [[i64; 500500]; 1000],
+    map: HashMap<(usize, usize), i64>,
 }
 
 // NOTE: getting references to work here ain't gonna be trivial :/. *That* is the learning I'll gather by solving this
-fn find_solution(problem: Problem, solutions: Solutions, i: u16, j: u16) {
-    // go find i, j-1 if not already found
-    // add P(i, j-1) to appropriate set of things in row j. might require getting column of each item?
-    // ya, b/c if we have col(s), then that's also the col to start with in row j!
+// pass mutable (borrow) references all the way down
+fn find_solution(problem: &Problem, solutions: &mut Solutions, i: usize, j: usize) -> i64 {
+    match solutions.map.get(&(i, j)) {
+        Some(solution) => return *solution,
+
+        None => {
+            let sol = if j == 0 {
+                problem.triangle[i]
+            } else {
+                let mut prev_solution: i64 = find_solution(problem, solutions, i, j - 1);
+
+                // NOTE: is there a way to avoid this casting?
+                let start_col = problem.cols[i];
+                let ju32 = j as u32;
+                let row = problem.rows[i] + ju32 + 1;
+
+                for i in 0..(j + 1) {
+                    prev_solution += problem.triangle[i + (start_col as usize)];
+                }
+
+                prev_solution
+            };
+
+            solutions.map.insert((i, j), sol);
+
+            sol
+        }
+    }
 }
 
 fn find_min_triangle(problem: Problem) -> i64 {
     // iterate through *all* solutions, find them, and keep track of the smallest one
-    0
+    let mut map: HashMap<(usize, usize), i64> = HashMap::new();
+    let mut solutions = Solutions { map };
+
+    let mut min_solution = std::i64::MAX;
+    let num_rows = problem.num_rows();
+    for i in 0..problem.size() {
+        for j in 0..(num_rows - i) {
+            let solution = find_solution(&problem, &mut solutions, i, j);
+            solutions.map.insert((i, j), solution);
+            if solution < min_solution {
+                min_solution = solution;
+            }
+        }
+    }
+
+    min_solution
 }
 
-// NOTE: it took like no time to generate all the nums. Next, the dp problem to get smallest triangle!
-// P(i, j) = sum of triangle rooted at i of side length j
-// P(i, j) = P(i, j-1) + row j in triangle
-// Total # of problems ~ Billions
-
-// Ohh, stackoverflow is happening b/c I'm allocating these massive arrays on stack. I should probably just be
-// allocating them on heap... i.e. vecs
 fn main() {
-    println!("Hello, world!");
-    println!("Done!");
+    println!("Running...");
+    let problem = generate_problem(500500);
+    println!("Generated problem");
+    let sol = find_min_triangle(problem);
+
+    println!("{}", sol)
 }
