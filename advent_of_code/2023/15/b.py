176c88bd748145e6c70b6933dabfff96843e75cc
lsankar4033 <lsankar4033@gmail.com>
2024-04-20 21:33:23 -0700

    advent of code 2023-15

--- advent_of_code/2023/15/b.py
+++ advent_of_code/2023/15/b.py
@@ -15,6 +15,9 @@ def calc_hash(label):
 with open('./input.txt', 'r') as file:
     lines = file.readlines()
 
+# lines = ['rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7']
+
+
 commands = lines[0].split(',')
 
 
@@ -27,21 +30,24 @@ def parse_label(cmd):
 
 Lens = namedtuple('Lens', ['label', 'focal_length'])
 
-boxes = [[]] * 256
+boxes = []
+for i in range(256):
+    boxes.append([])
 for command in commands:
     label = parse_label(command)
     box = calc_hash(label)
 
     if '=' in command:
-        old_box = boxes[box]
-        if not any(l.label == label for l in old_box):
+        focal_length = int(command.split('=')[1])
+        if not any(l.label == label for l in boxes[box]):
             boxes[box].append(
-                Lens(label=label, focal_length=int(command.split('=')[1])))
+                Lens(label=label, focal_length=focal_length))
 
         else:
-            old_ind = [i for i, l in enumerate(old_box) if l.label == label][0]
+            old_ind = [i for i, l in enumerate(
+                boxes[box]) if l.label == label][0]
             boxes[box][old_ind] = Lens(
-                label=label, focal_length=int(command.split('=')[1]))
+                label=label, focal_length=focal_length)
 
     elif '-' in command:
         old_box = boxes[box]
